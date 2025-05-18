1e46088adf6a544fc9369f0e0fd558a52be2b332
TecnoKiller <leodrik.unam@gmail.com>
2025-05-18 15:58:53 -0600

    Problema1.c

--- Problema1.c
+++ Problema1.c
@@ -2,23 +2,19 @@
 #include <stdio.h>
 
 int main() {
-    // Declaración de variables
     float num1, num2;
     float suma, resta, multiplicacion;
 
-    // Entrada de datos
     printf("Ingrese el primer número: ");
     scanf("%f", &num1);
 
     printf("Ingrese el segundo número: ");
     scanf("%f", &num2);
 
-    // Cálculos
     suma = num1 + num2;
     resta = num1 - num2;
     multiplicacion = num1 * num2;
 
-    // Salida de resultados
     printf("Suma: %.2f\n", suma);
     printf("Resta: %.2f\n", resta);
     printf("Multiplicación: %.2f\n", multiplicacion);
