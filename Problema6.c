9e9abc719875725a46b1c0fa3726cfd6fb1eaa30
TecnoKiller <leodrik.unam@gmail.com>
2025-05-18 16:03:18 -0600

    Problema6.c

--- Problema6.c
+++ Problema6.c
@@ -11,24 +11,20 @@ int main()
 {
     float sueldo, aumento, nuevoSueldo;
 
-    // Entrada de datos
     printf("Ingrese el sueldo del trabajador: ");
     scanf("%f", &sueldo);
 
-    // Cálculo del aumento según el sueldo
     if (sueldo < 1000) 
     {
-        aumento = sueldo * 0.15; // 15% de aumento
+        aumento = sueldo * 0.15; 
     } 
     else 
     {
-        aumento = sueldo * 0.12; // 12% de aumento
+        aumento = sueldo * 0.12; 
     }
 
-    // Calcular nuevo sueldo
     nuevoSueldo = sueldo + aumento;
 
-    // Mostrar el resultado
     printf("El nuevo sueldo con el aumento es: %.2f\n", nuevoSueldo);
 
     return 0;
