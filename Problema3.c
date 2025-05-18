93feab403303a7444f2398edda8dabb65be8c10e
TecnoKiller <leodrik.unam@gmail.com>
2025-05-18 13:00:37 -0600

    Problema3.c

---
+++ Problema3.c
@@ -0,0 +1,25 @@
+/*
+En una Casa de Cabio necesitan construir un programa que dado como
+dato una cantidad expresada en d ́olares, convierta esa cantidad en pesos.
+Construir el diagrama de flujo correspondiente. Tomar en cuenta que:
+1dolar ↢ 11.96pesos.
+*/
+#include <stdio.h>
+
+int main() 
+{
+    float dolares, pesos;
+    const float TIPO_CAMBIO = 11.96;
+
+    // Entrada de datos
+    printf("Ingrese la cantidad en dólares: ");
+    scanf("%f", &dolares);
+
+    // Conversión
+    pesos = dolares * TIPO_CAMBIO;
+
+    // Salida
+    printf("Equivalente en pesos: %.2f\n", pesos);
+
+    return 0;
+}
